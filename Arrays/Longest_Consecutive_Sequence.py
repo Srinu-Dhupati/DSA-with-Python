ffd6a8427165345c095c481863dd9f82b3a29e7b
Srinu-Dhupati <dhupatisrinivasu@gmail.com>
2025-12-03 17:19:07 +0530

    Day24:Today I Solved Longest Consecutive Sequence problem

---
+++ Arrays/Longest_Consecutive_Sequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numset=set(nums)
+        longest=0
+        for i in numset:
+            if (i-1) not in numset:
+                current=i
+                length=1
+                while (current+1) in numset:
+                    current+=1
+                    length+=1
+                longest=max(length,longest)
+        return longest
